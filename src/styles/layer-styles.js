3c63239d2a1ec7f9d26cf877a9cc8091cee5cafc
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-10-09 09:30:55 -0300

    Separating text and layout styles of navMenuButtonAndList.

--- src/styles/layer-styles.js
+++ src/styles/layer-styles.js
@@ -23,9 +23,6 @@ export const layerStyles = defineLayerStyles({
     description:
       "The basic styles for the button and list itens, so they have the same layout.",
     value: {
-      fontFamily: "DirectaSerifSemiBold, Georgia, serif",
-      fontSize: ["1rem", undefined, "1.2rem"],
-      lineHeight: ["20px", undefined, "24px"],
       p: ["10px", undefined, undefined, "10px 30px"],
     },
   },
--- src/styles/text-styles.js
+++ src/styles/text-styles.js
@@ -1,3 +1,13 @@
 import { defineTextStyles } from "@pandacss/dev"
 
-export const textStyles = defineTextStyles({})
+export const textStyles = defineTextStyles({
+  navMenuButtonAndList: {
+    description:
+      "The basic styles for the button and list itens, so they have the same layout.",
+    value: {
+      fontFamily: "DirectaSerifSemiBold, Georgia, serif",
+      fontSize: ["1rem", undefined, "1.2rem"],
+      lineHeight: ["20px", undefined, "24px"],
+    },
+  },
+})
