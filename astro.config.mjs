c9f69213ff0d7f6d975d54ce1e950c419733865f
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-10-13 15:45:08 -0300

    Astro Page Insight and astro-meta-tags installed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config"
 
 import mdx from "@astrojs/mdx"
 
+import pageInsight from "astro-page-insight";
+
+import metaTags from "astro-meta-tags";
+
 // https://astro.build/config
 export default defineConfig({
   vite: { server: { allowedHosts: ["macleod.local"] } },
   site: "https://www.oficiofebril.com.br",
-  integrations: [mdx()],
+  integrations: [mdx(), pageInsight(), metaTags()],
 })
