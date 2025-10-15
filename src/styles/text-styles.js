a03433ca3088f6e3d965dde2e700ad52d8b891e2
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2025-10-15 11:55:26 -0300

    Creating processos artísticos section.

--- src/styles/text-styles.js
+++ src/styles/text-styles.js
@@ -23,24 +23,24 @@ export const textStyles = defineTextStyles({
     description: "The basic styles for content h2 headings.",
     value: {
       fontFamily: "DirectaSerifSemiBold, Georgia, serif",
-      fontSize: ["1.4rem", undefined, "1.8rem"],
-      lineHeight: ["28px", undefined, "36px"],
+      fontSize: ["1.6rem", undefined, "2rem"],
+      lineHeight: ["32px", undefined, "40px"],
     },
   },
   basicHeading3: {
     description: "The basic styles for content h3 headings.",
     value: {
       fontFamily: "DirectaSerifSemiBold, Georgia, serif",
-      fontSize: ["1.2rem", undefined, "1.4rem"],
-      lineHeight: ["24px", undefined, "28px"],
+      fontSize: ["1.4rem", undefined, "1.6rem"],
+      lineHeight: ["28px", undefined, "32px"],
     },
   },
   basicHeading4: {
     description: "The basic styles for content h4 headings.",
     value: {
       fontFamily: "DirectaSerifSemiBold, Georgia, serif",
-      fontSize: ["1rem", undefined, "1.2rem"],
-      lineHeight: ["24px", undefined, "28px"],
+      fontSize: ["1.2rem", undefined, "1.4rem"],
+      lineHeight: ["28px", undefined, "32px"],
     },
   },
   basicText: {
@@ -49,6 +49,15 @@ export const textStyles = defineTextStyles({
       fontFamily: "DirectaSerifLight, Georgia, serif",
       fontSize: ["1rem", undefined, "1.2rem"],
       lineHeight: ["24px", undefined, "28px"],
+      "& a": { textDecoration: "underline" },
+    },
+  },
+  spanText: {
+    description: "The basic styles for content span.",
+    value: {
+      fontFamily: "DirectaSerifLight, Georgia, serif",
+      fontSize: ["0.8rem", undefined, "1rem"],
+      lineHeight: ["20px", undefined, "24px"],
       "& a": { textDecoration: "underline" },
     },
   },
