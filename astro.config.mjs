ce5a39a4282798c953cf424e391d57db21379be0
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2026-04-06 12:00:02 -0300

    Packages updated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from "astro/config"
 
 import mdx from "@astrojs/mdx"
 
-import pageInsight from "astro-page-insight";
+import metaTags from "astro-meta-tags"
 
-import metaTags from "astro-meta-tags";
-
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   vite: { server: { allowedHosts: ["macleod.local"] } },
   site: "https://www.oficiofebril.com.br",
-  integrations: [mdx(), pageInsight(), metaTags(), sitemap()],
+  integrations: [mdx(), metaTags(), sitemap()],
 })
